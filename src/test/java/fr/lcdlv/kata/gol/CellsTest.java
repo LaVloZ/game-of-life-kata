7e760b3ad6a45078cec9a6234b89e0707b8c8d7d
Abdessamed Mansouri <Abdessamed.mansouri@lacombedulionvert.fr>
2021-01-10 22:10:28 +0100

    Test de documentation

--- src/test/java/fr/lcdlv/kata/gol/CellsTest.java
+++ src/test/java/fr/lcdlv/kata/gol/CellsTest.java
@@ -1,6 +1,7 @@
 package fr.lcdlv.kata.gol;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -10,18 +11,22 @@ public class CellsTest {
     private static final boolean ALIVE = true;
     private static final boolean DEAD = false;
 
-    @Test
-    public void update_returns_false_when_no_alive_neighbours() {
-        boolean deadCell = update(ALIVE, 0);
+    @Nested
+    public class Death {
 
-        assertThat(deadCell).isEqualTo(DEAD);
-    }
+        @Test
+        public void update_returns_false_when_no_alive_neighbours() {
+            boolean deadCell = update(ALIVE, 0);
 
-    @Test
-    public void update_returns_false_when_only_one_alive_neighbour() {
-        boolean deadCell = update(ALIVE, 1);
+            assertThat(deadCell).isEqualTo(DEAD);
+        }
 
-        assertThat(deadCell).isEqualTo(DEAD);
+        @Test
+        public void update_returns_false_when_only_one_alive_neighbour() {
+            boolean deadCell = update(ALIVE, 1);
+
+            assertThat(deadCell).isEqualTo(DEAD);
+        }
     }
 
     private boolean update(boolean cell, int aliveNeighbours) {
