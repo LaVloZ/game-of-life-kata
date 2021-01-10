3799456f9786d810991eeafd229b6eeccb7bc178
Abdessamed Mansouri <Abdessamed.mansouri@lacombedulionvert.fr>
2021-01-10 22:43:42 +0100

    refactor

--- src/main/java/fr/lcdlv/kata/gol/Cell.java
+++ src/main/java/fr/lcdlv/kata/gol/Cell.java
@@ -1,10 +1,14 @@
 package fr.lcdlv.kata.gol;
 
 public class Cell {
+
+    private static final int TWO_ALIVE_NEIGHBOURS = 2;
+    private static final int THREE_ALIVE_NEIGHBOURS = 3;
+
     static boolean update(boolean aliveCell, int aliveNeighbours) {
         if (aliveCell) {
-            return aliveNeighbours == 2 || aliveNeighbours == 3;
+            return aliveNeighbours == TWO_ALIVE_NEIGHBOURS || aliveNeighbours == THREE_ALIVE_NEIGHBOURS;
         }
-        return aliveNeighbours == 3;
+        return aliveNeighbours == THREE_ALIVE_NEIGHBOURS;
     }
 }
