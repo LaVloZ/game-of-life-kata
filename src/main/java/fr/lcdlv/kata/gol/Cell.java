f4106c51cb5aee9c4fffe590ebfda6e660d22cb8
Abdessamed Mansouri <Abdessamed.mansouri@lacombedulionvert.fr>
2021-01-10 22:49:14 +0100

    refactor

--- src/main/java/fr/lcdlv/kata/gol/Cell.java
+++ src/main/java/fr/lcdlv/kata/gol/Cell.java
@@ -5,7 +5,7 @@ public class Cell {
     private static final int TWO_ALIVE_NEIGHBOURS = 2;
     private static final int THREE_ALIVE_NEIGHBOURS = 3;
 
-    private boolean alive;
+    private final boolean alive;
 
     public Cell(boolean alive) {
         this.alive = alive;
