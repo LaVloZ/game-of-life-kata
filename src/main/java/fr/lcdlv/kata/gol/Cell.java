70918c0f41e95fb67f599a960cfddb24eda99659
Abdessamed Mansouri <Abdessamed.mansouri@lacombedulionvert.fr>
2021-01-11 01:20:32 +0100

    Refactor

--- src/main/java/fr/lcdlv/kata/gol/Cell.java
+++ src/main/java/fr/lcdlv/kata/gol/Cell.java
@@ -5,16 +5,5 @@ public class Cell {
     private static final int TWO_ALIVE_NEIGHBOURS = 2;
     private static final int THREE_ALIVE_NEIGHBOURS = 3;
 
-    private final boolean alive;
-
-    public Cell(State state) {
-        this.alive = state.isAlive();
-    }
-
-    public boolean update(int aliveNeighbours) {
-        if (alive) {
-            return aliveNeighbours == TWO_ALIVE_NEIGHBOURS || aliveNeighbours == THREE_ALIVE_NEIGHBOURS;
-        }
-        return aliveNeighbours == THREE_ALIVE_NEIGHBOURS;
-    }
+    public abstract Cell update(int aliveNeighbours);
 }
--- src/test/java/fr/lcdlv/kata/gol/CellTest.java
+++ src/test/java/fr/lcdlv/kata/gol/CellTest.java
@@ -3,12 +3,11 @@ package fr.lcdlv.kata.gol;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static fr.lcdlv.kata.gol.Cell.ALIVE;
+import static fr.lcdlv.kata.gol.Cell.DEAD;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CellTest {
-
-    private static final boolean ALIVE = true;
-    private static final boolean DEAD = false;
 
     @Nested
     public class Death {
@@ -17,14 +16,14 @@ public class CellTest {
         public class Underpopulation {
             @Test
             public void update_returns_dead_when_alive_cell_without_alive_neighbours() {
-                boolean deadCell = new Cell(new State(ALIVE)).update(0);
+                Cell deadCell = ALIVE.update(0);
 
                 assertThat(deadCell).isEqualTo(DEAD);
             }
 
             @Test
             public void update_returns_dead_when_alive_cell_with_only_one_alive_neighbour() {
-                boolean deadCell = new Cell(new State(ALIVE)).update(1);
+                Cell deadCell = ALIVE.update(1);
 
                 assertThat(deadCell).isEqualTo(DEAD);
             }
@@ -34,7 +33,7 @@ public class CellTest {
         public class Overcrowding {
             @Test
             public void update_returns_dead_when_alive_cell_with_four_alive_neighbours() {
-                boolean deadCell = new Cell(new State(ALIVE)).update(4);
+                Cell deadCell = ALIVE.update(4);
 
                 assertThat(deadCell).isEqualTo(DEAD);
             }
@@ -45,28 +44,28 @@ public class CellTest {
     public class Life {
         @Test
         public void update_return_alive_when_alive_cell_with_two_alive_neighbours() {
-            boolean deadCell = new Cell(new State(ALIVE)).update(2);
+            Cell deadCell = ALIVE.update(2);
 
             assertThat(deadCell).isEqualTo(ALIVE);
         }
 
         @Test
         public void update_return_alive_when_alive_cell_with_three_alive_neighbours() {
-            boolean deadCell = new Cell(new State(ALIVE)).update(3);
+            Cell deadCell = ALIVE.update(3);
 
             assertThat(deadCell).isEqualTo(ALIVE);
         }
 
         @Test
         public void update_return_alive_when_dead_cell_with_three_alive_neighbours() {
-            boolean deadCell = new Cell(new State(DEAD)).update(3);
+            Cell deadCell = DEAD.update(3);
 
             assertThat(deadCell).isEqualTo(ALIVE);
         }
 
         @Test
         public void update_return_dead_when_dead_cell_with_two_alive_neighbours() {
-            boolean deadCell = new Cell(new State(DEAD)).update(2);
+            Cell deadCell = DEAD.update(2);
 
             assertThat(deadCell).isEqualTo(DEAD);
         }
