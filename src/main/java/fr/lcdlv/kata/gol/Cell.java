e34f206e4de3f66b1d5e4f3a70a789d6bd149713
Abdessamed Mansouri <Abdessamed.mansouri@lacombedulionvert.fr>
2021-01-11 01:21:31 +0100

    Refactor

--- src/main/java/fr/lcdlv/kata/gol/Cell.java
+++ src/main/java/fr/lcdlv/kata/gol/Cell.java
@@ -1,6 +1,18 @@
 package fr.lcdlv.kata.gol;
 
-public class Cell {
+public enum Cell {
+
+    ALIVE {
+        @Override
+        public Cell update(int aliveNeighbours) {
+            return aliveNeighbours == TWO_ALIVE_NEIGHBOURS || aliveNeighbours == THREE_ALIVE_NEIGHBOURS ? ALIVE : DEAD;
+        }
+    }, DEAD {
+        @Override
+        public Cell update(int aliveNeighbours) {
+            return aliveNeighbours == THREE_ALIVE_NEIGHBOURS ? ALIVE : DEAD;
+        }
+    };
 
     private static final int TWO_ALIVE_NEIGHBOURS = 2;
     private static final int THREE_ALIVE_NEIGHBOURS = 3;
