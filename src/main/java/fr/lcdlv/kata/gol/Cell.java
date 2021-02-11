920ea2835972529e6595cc2315ab5a336a0dddec
Abdessamed Mansouri <Abdessamed.mansouri@lacombedulionvert.fr>
2021-02-12 00:15:14 +0100

    Refacto

--- src/main/java/fr/lcdlv/kata/gol/Cell.java
+++ src/main/java/fr/lcdlv/kata/gol/Cell.java
@@ -1,7 +1,5 @@
 package fr.lcdlv.kata.gol;
 
-import fr.lcdlv.kata.gol.Cells.Neighbours;
-
 public enum Cell {
 
     ALIVE {
@@ -38,11 +36,6 @@ public enum Cell {
 
     private static final int TWO_ALIVE_NEIGHBOURS = 2;
     private static final int THREE_ALIVE_NEIGHBOURS = 3;
-
-    public Cell nextGeneration(Neighbours neighbours) {
-        var aliveNeighbours = neighbours.countAlive(0);
-        return nextGeneration(aliveNeighbours);
-    }
 
     abstract Cell nextGeneration(int aliveNeighbours);
     public abstract int countAlive(int counter);
--- src/main/java/fr/lcdlv/kata/gol/Cells.java
+++ src/main/java/fr/lcdlv/kata/gol/Cells.java
@@ -37,7 +37,7 @@ public class Cells {
         Cell cell = grid[row][column];
         var neighbours = new Neighbours(row, column);
 
-        return cell.nextGeneration(neighbours);
+        return neighbours.nextGeneration(cell);
     }
 
     class Neighbours {
@@ -73,7 +73,12 @@ public class Cells {
             return row >= 0 && row < grid.length && column >= 0 && column < grid[row].length;
         }
 
-        public int countAlive(int counter) {
+        public Cell nextGeneration(Cell cell) {
+            var aliveNeighbours = countAlive(0);
+            return cell.nextGeneration(aliveNeighbours);
+        }
+
+        private int countAlive(int counter) {
             return cells.stream()
                     .filter(Optional::isPresent)
                     .map(Optional::get)
